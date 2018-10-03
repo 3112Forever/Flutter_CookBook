62ccf3425b27ae9770c79afcb1f99af9a635f32d
3112Forever <jknoproblem3112@gail.com>
2018-10-03 10:45:15 +0530

    Snackbar Demo...

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,47 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(SnackbarDemo());
 
-class MyApp extends StatelessWidget {
+class SnackbarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appName = 'Custom Themes';
-
     return MaterialApp(
-      title: appName,
+      title: 'Snackbar Demo',
       theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.redAccent, // Appbar
-        accentColor: Colors.red, //
+        primaryColor: Colors.redAccent,
+        accentColor: Colors.red,
       ),
-      home: MyHomePage(
-        title: appName,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Snackbar Demo'),
+        ),
+        body: SnackBarDemo(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
 
-  MyHomePage({Key key, @required this.title}) : super(key: key);
-
+class SnackBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Snackbar Demo....'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change!
+              },
+            ),
+          );
 
-
-      body: Center(
-        child: Container(
-          color: Theme.of(context).accentColor,
-          child: Text(
-            'Text with a background color',
-            style: Theme.of(context).textTheme.title,
-          ),
-        ),
-      ),
-
-
-      // Button Color Change
-      floatingActionButton: Theme(
-        data: Theme.of(context).copyWith(accentColor: Colors.red),
-        child: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.add),
-        ),
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Show SnackBar'),
       ),
     );
   }
